ad755a7105e455979e8ca05361c72736f24ad2b4
Hasan Naseem <hasannaseem2@gmail.com>
2022-11-15 00:15:40 +0500

    Buttons added in rows

--- src/table/index.tsx
+++ src/table/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC, ReactElement } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { createTheme } from "@mui/material/styles";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { Button } from "@mui/material";
 
 interface tableData {
   contactName: string;
@@ -47,6 +49,10 @@ const rowData: tableData[] = [
   },
 ];
 
+const handleOnClickEvent = () => {
+  console.log("Hello World");
+};
+
 export const UserTable: FC = (): ReactElement => {
   const theme = createTheme({
     typography: {
@@ -54,16 +60,18 @@ export const UserTable: FC = (): ReactElement => {
     },
   });
   return (
-    // <ThemeProvider theme={theme}>
-    //   </ThemeProvider>
-
     <TableContainer
       component={Paper}
-      sx={{ border: 1, borderWidth: 3, borderRadius: 4.5 }}
+      sx={{
+        border: 1,
+        borderWidth: 3,
+        borderRadius: 4.5,
+      }}
     >
-      <Table>
+      <Table sx={{ borderCollapse: "collapse" }}>
         <TableHead>
-          <TableRow>
+          <TableRow sx={{ borderInlineEnd: "inherit" }}>
+            <TableCell></TableCell>
             <TableCell
               sx={{
                 fontFamily: "Rothern Variable",
@@ -106,6 +114,8 @@ export const UserTable: FC = (): ReactElement => {
               {" "}
               Date
             </TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -114,6 +124,25 @@ export const UserTable: FC = (): ReactElement => {
               key={row.quotationId}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
+              <TableCell sx={{ height: 80, width: 80 }}>
+                <span
+                  style={{
+                    fontWeight: "bold",
+                    backgroundColor: "grey",
+                    display: "block",
+                    height: 20,
+                    width: 20,
+                    borderRadius: "50%",
+                    border: "5 solid #000",
+                    margin: "auto",
+                    textAlign: "center",
+                    position: "relative",
+                    fontSize: 13,
+                  }}
+                >
+                  {row.contactName.charAt(0).toUpperCase()}
+                </span>
+              </TableCell>
               <TableCell
                 component="th"
                 scope="row"
@@ -137,6 +166,18 @@ export const UserTable: FC = (): ReactElement => {
               </TableCell>
               <TableCell sx={{ fontFamily: "Rothern Variable" }}>
                 {row.date}
+              </TableCell>
+              <TableCell>
+                <RemoveRedEyeIcon onClick={handleOnClickEvent} />
+              </TableCell>
+              <TableCell>
+                <Button
+                  variant="outlined"
+                  sx={{ bgcolor: "yellow" }}
+                  onClick={handleOnClickEvent}
+                >
+                  Build Invoice
+                </Button>
               </TableCell>
             </TableRow>
           ))}
